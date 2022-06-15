97e2a2bb1de507f05dc40bf359dbfb0af1121367
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-06-15 16:22:44 +0300

    feat: Ark Oracle address v0.0.2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 import "./setEnv.js";
-export const ARWEAVE_ORACLE_ADDRESS = "YPf5wvXXnVk4Ats9CpllqlwdmwFHTZc1x-w4H1tz0no" // Arweave Mainnet
+// export const ARWEAVE_ORACLE_ADDRESS = "YPf5wvXXnVk4Ats9CpllqlwdmwFHTZc1x-w4H1tz0no" ; v0.0.1
+export const ARWEAVE_ORACLE_ADDRESS = "Ukw2TLpWw5lqKD6LVpLE8cx4hSZKeGmSLjBfZqUFs08"; // Arweave Mainnet
 export const EVM_ORACLE_ADDRESS = "0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A"; // Goerli Testnet
 export const RPC_PROVIDER_URL = "http://127.0.0.1:9545/";
 export const GOERLI_RPC = `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`
