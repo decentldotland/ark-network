7eeb3deca479cd0c00f90649e8fc5d8638b15266
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-01 20:33:06 +0300

    feat: handling multi-network verification

--- src/utils/evm/ethers.js
+++ src/utils/evm/ethers.js
@@ -1,22 +1,47 @@
 import { ethers } from "ethers";
-import { RPC_PROVIDER_URL, EVM_ORACLE_ADDRESS, GOERLI_RPC } from "../constants.js";
+import {
+  ETH_ORACLE_ADDRESS,
+  AURORA_TESTNET_ADDRESS,
+  GOERLI_ETH_RPC,
+  MAINNET_ETH_RPC,
+  AURORA_TESTNET_RPC,
+} from "../constants.js";
 import { ArkNetwork } from "./web3.js";
 
-const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC);
 const decoder = new ethers.utils.AbiCoder();
 
-// The Contract object
-const ArkContract = new ethers.Contract(
-  EVM_ORACLE_ADDRESS,
-  ArkNetwork.abi,
-  provider
-);
 
-export async function getTransaction(txid) {
+export async function getTransaction(txid, network) {
   try {
+    const { RPC_URL, CONTRACT_ADDRESS } = await resolveNetworkKey(network);
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
+    const ArkContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      ArkNetwork.abi,
+      provider
+    );
     const tx = await provider.getTransactionReceipt(txid);
     return tx;
   } catch (error) {
     console.log(error);
+  }
+}
+
+async function resolveNetworkKey(network_key) {
+  switch (network_key) {
+    case "ETH-GOERLI":
+      return { RPC_URL: GOERLI_ETH_RPC, CONTRACT_ADDRESS: ETH_ORACLE_ADDRESS };
+
+    case "ETH-MAINNET":
+      return { RPC_URL: MAINNET_ETH_RPC, CONTRACT_ADDRESS: ETH_ORACLE_ADDRESS };
+
+    case "AURORA-TESTNET":
+      return {
+        RPC_URL: AURORA_TESTNET_RPC,
+        CONTRACT_ADDRESS: AURORA_TESTNET_ADDRESS,
+      };
+    default:
+      return { RPC_URL: GOERLI_ETH_RPC, CONTRACT_ADDRESS: ETH_ORACLE_ADDRESS };
   }
 }
