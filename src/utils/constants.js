a5401cc9806b91a8ca9bd5e468f66d7d00552a84
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-11 13:26:47 +0300

    chore: add oracle v0.0.6 SWCID

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@ import "./setEnv.js";
 // Arweave Contracts
 // export const ARWEAVE_ORACLE_ADDRESS = "qP614umsvOo9Szvl-xqvnXH0xLOg2eKOsLYnKx2l5SA" // v0.0.3
 export const ARWEAVE_ORACLE_ADDRESS = "i9Q9Y14HnJUmnSOVdxlPQkHHsT0W6kTv9PXCblZ_kAA"; // v0.0.4
+// export const ARWEAVE_ORACLE_ADDRESS = "jRGJtaBjfvDJgpQATUiW3mBbB_wp71xrUmeQBalrm3k" // v0.0.6
 // EVM ADDRESSES
 export const ETH_ORACLE_ADDRESS = "0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A"; // Goerli Testnet && ETH Mainnet
 export const AURORA_TESTNET_ADDRESS = "0xfb0200C27185185D7DEe0403D5f102ADb59B7c34";
