1ccbb942b4fc4c060acee925faeb09c90a3e2974
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-26 16:02:14 +0300

    feat: Ark profile endpoint

--- src/app.js
+++ src/app.js
@@ -1,46 +1,71 @@
-import { web3, ArkNetwork, checkTopicAgainstAddress } from "./utils/evm/web3.js";
+import {
+  web3,
+  ArkNetwork,
+  checkTopicAgainstAddress,
+} from "./utils/evm/web3.js";
 import { sleepBlockCount } from "./utils/arweave/network.js";
-import  { getTransaction } from "./utils/evm/ethers.js";
-import { getOracleState, getStats, getNetworkAddresses } from "./utils/cache-utils.js";
+import { getTransaction } from "./utils/evm/ethers.js";
+import {
+  getOracleState,
+  getStats,
+  getNetworkAddresses,
+} from "./utils/cache-utils.js";
 import { runPolling } from "./utils/polling.js";
-import sha256 from "sha256"
+import { getArkProfile } from "./utils/server-utils.js";
+import sha256 from "sha256";
 import express from "express";
 import base64url from "base64url";
-import cors  from "cors";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 
-app.use(express.static("public"))
-
-app.use(cors({
-    origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.set("view engine", "ejs");
 
 app.get("/ark/oracle/state", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  const encodedState = (await getOracleState())?.res
+  const encodedState = (await getOracleState())?.res;
+  if (!encodedState) {
+    res.send(JSON.parse(`{}`));
+    return;
+  }
   const jsonRes = JSON.parse(base64url.decode(encodedState));
   res.send(jsonRes);
 });
 
 app.get("/ark/network/stats", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  const stats = await getStats()
+  const stats = await getStats();
   res.send(stats);
 });
 
 app.get("/ark/network/addresses", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  const addresses = await getNetworkAddresses()
+  const addresses = await getNetworkAddresses();
   res.send(addresses);
 });
 
+app.get("/ark/profile/:arweave_address", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const profile = await getArkProfile(req.params.arweave_address);
+  if (!profile) {
+    res.send(JSON.parse(`{}`));
+    return;
+  }
+  const jsonRes = JSON.parse(base64url.decode(profile));
+  res.send(jsonRes);
+});
+
 app.listen(port, async () => {
-  while(true) {
+  while (true) {
     await runPolling();
     await sleepBlockCount(2);
     console.log(`listening at PORT:${port}`);
