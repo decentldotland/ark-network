1c73fa4bde4f8ba0033ae09c7bac9030cd39dc82
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-30 03:13:02 +0300

    feat: add Arbitrum SC & RPC

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,6 +15,7 @@ export const AVALANCHE_MAINNET_ADDRESS = `0xE5E0A3380811aD9380F91a6996529da0a262
 export const BSC_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
 export const FTM_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
 export const OPTIMISM_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
+export const ARBITRUM_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
 
 export const EVM_ORACLES_CONTRACTS = [
   ETH_ORACLE_ADDRESS,
@@ -26,6 +27,7 @@ export const EVM_ORACLES_CONTRACTS = [
   BSC_MAINNET_ADDRESS,
   FTM_MAINNET_ADDRESS,
   OPTIMISM_MAINNET_ADDRESS,
+  ARBITRUM_MAINNET_ADDRESS,
 ];
 
 // RPCs
@@ -40,6 +42,7 @@ export const AVALANCHE_MAINNET_RPC = `https://api.avax.network/ext/bc/C/rpc`;
 export const NEON_DEVNET_RPC = `https://proxy.devnet.neonlabs.org/solana`;
 export const FTM_MAINNET_RPC = `https://rpc.ftm.tools/`;
 export const OPTIMISM_MAINNET_RPC = `https://mainnet.optimism.io`;
+export const ARBITRUM_MAINNET_RPC = `https://arb1.arbitrum.io/rpc`;
 export const RPC_PORT = 9545;
 
 // APIs
