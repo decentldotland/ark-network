5d6d3dd84a450a78c1a4b04d89999a6f9065f547
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-08-05 22:52:26 +0300

    feat: verify exotic identities

--- src/utils/arweave/smartweave.js
+++ src/utils/arweave/smartweave.js
@@ -5,6 +5,7 @@ import { SmartWeaveNodeFactory, LoggerFactory } from "redstone-smartweave";
 import { verifyUser } from "../../telegram/verify.js";
 import { red, green } from "../chalk.js";
 import { arweave } from "./network.js";
+import { canBeVerifiedNear } from "../near/config.js";
 import "../setEnv.js";
 
 const smartweave = SmartWeaveNodeFactory.memCached(arweave);
@@ -27,83 +28,136 @@ export async function checkAndVerifyUser(userObject) {
     const {
       arweave_address,
       evm_address,
+      is_evaluated,
       verification_req,
       identity_id,
       telegram_username,
       ver_req_network,
+      has_unevaluated_exotic_addrs,
+      exotic_addresses,
     } = userObject;
-    const evmVerificationReq = await getTransaction(
-      verification_req,
-      ver_req_network
-    );
+    // if the verification is on an EVM network
+    if (!is_evaluated && !has_unevaluated_exotic_addrs) {
+      const evmVerificationReq = await getTransaction(
+        verification_req,
+        ver_req_network
+      );
+      if (!EVM_ORACLES_CONTRACTS.includes(evmVerificationReq.to)) {
+        throw new Error(`invalid contract interaction/address`);
+      }
 
-    if (!EVM_ORACLES_CONTRACTS.includes(evmVerificationReq.to)) {
-      throw new Error(`invalid contract interaction/address`);
+      const hashedArAddressLog = evmVerificationReq.logs[0].topics[2];
+
+      const validity = await checkTopicAgainstAddress(
+        hashedArAddressLog,
+        arweave_address
+      );
+
+      const identityValidity = validity.is_equal ? true : false;
+
+      const tx = await arweave.createTransaction(
+        {
+          data: "Ark Network verification",
+        },
+        pk
+      );
+
+      const input = `{"function": "verifyIdentity", "identityOf": "${arweave_address}", "verificationReq": "${verification_req}", "validity": ${identityValidity}}`;
+
+      tx.addTag("App-Name", "SmartWeaveAction");
+      tx.addTag("App-Version", "0.3.0");
+      tx.addTag("Contract", ARWEAVE_ORACLE_ADDRESS);
+      tx.addTag("Input", input);
+      tx.addTag("Protocol-Name", "Ark-Network");
+      tx.addTag("Protocol-Action", "Verify-Identity");
+      tx.addTag("Identity-ID", identity_id);
+      tx.addTag("Content-Type", "text/plain");
+
+      tx.reward = (+tx.reward * 100).toString();
+
+      await arweave.transactions.sign(tx, pk);
+      await arweave.transactions.post(tx);
+
+      if (identityValidity) {
+        console.log(
+          green(
+            `\n\n--> identity verified: ${arweave_address} <-> ${evm_address} || network: ${userObject.ver_req_network}`
+          )
+        );
+        console.log(
+          green(
+            `--> verified identity ID ${identity_id} | verification TXID: ${tx.id}\n\n`
+          )
+        );
+      } else {
+        console.log(
+          red(
+            `\n\n--> verification failed: ${arweave_address} <-> ${evm_address} || network: ${userObject.ver_req_network}`
+          )
+        );
+        console.log(
+          red(
+            `--> failed identity ID ${identity_id} | failing verification TXID: ${tx.id}\n\n`
+          )
+        );
+      }
     }
-
-    // if (telegram_username) {
-    //   const isValid = await verifyUser(identity_id, telegram_username);
-
-    //   if (!isValid) {
-    //     throw new Error(`Failed while verifying Telegram identity`);
-    //   }
-    //   console.log(green(`Telegram identity valid --> ${telegram_username}`));
-    // }
-
-    const hashedArAddressLog = evmVerificationReq.logs[0].topics[2];
-
-    const validity = await checkTopicAgainstAddress(
-      hashedArAddressLog,
-      arweave_address
-    );
-
-    const identityValidity = validity.is_equal ? true : false;
-
-    const tx = await arweave.createTransaction(
-      {
-        data: "Ark Network verification",
-      },
-      pk
-    );
-
-    const input = `{"function": "verifyIdentity", "identityOf": "${arweave_address}", "validity": ${identityValidity}}`;
-
-    tx.addTag("App-Name", "SmartWeaveAction");
-    tx.addTag("App-Version", "0.3.0");
-    tx.addTag("Contract", ARWEAVE_ORACLE_ADDRESS);
-    tx.addTag("Input", input);
-    tx.addTag("Protocol-Name", "Ark-Network");
-    tx.addTag("Protocol-Action", "Verify-Identity");
-    tx.addTag("Identity-ID", identity_id);
-    tx.addTag("Content-Type", "text/plain");
-
-    tx.reward = (+tx.reward * 100).toString();
-
-    await arweave.transactions.sign(tx, pk);
-    await arweave.transactions.post(tx);
-
-    if (identityValidity) {
-      console.log(
-        green(
-          `\n\n--> identity verified: ${arweave_address} <-> ${evm_address} || network: ${userObject.ver_req_network}`
-        )
+    // if the verification is on an Exotic network
+    if (has_unevaluated_exotic_addrs) {
+      const unevaluatedAddr = exotic_addresses.find(
+        (addr) => !addr.is_evaluated
       );
-      console.log(
-        green(
-          `--> verified identity ID ${identity_id} | verification TXID: ${tx.id}\n\n`
-        )
+      const isVerifiable = await canBeVerifiedNear({
+        arweave_address: arweave_address,
+        verificationReq: unevaluatedAddr.verification_req,
+        exotic_address: unevaluatedAddr.exotic_address,
+      });
+      const tx = await arweave.createTransaction(
+        {
+          data: "Ark Network verification",
+        },
+        pk
       );
-    } else {
-      console.log(
-        red(
-          `\n\n--> verification failed: ${arweave_address} <-> ${evm_address} || network: ${userObject.ver_req_network}`
-        )
-      );
-      console.log(
-        red(
-          `--> failed identity ID ${identity_id} | failing verification TXID: ${tx.id}\n\n`
-        )
-      );
+
+      const input = `{"function": "verifyIdentity", "identityOf": "${arweave_address}", "verificationReq": "${unevaluatedAddr.verification_req}", "validity": ${isVerifiable}}`;
+
+      tx.addTag("App-Name", "SmartWeaveAction");
+      tx.addTag("App-Version", "0.3.0");
+      tx.addTag("Contract", ARWEAVE_ORACLE_ADDRESS);
+      tx.addTag("Input", input);
+      tx.addTag("Protocol-Name", "Ark-Network");
+      tx.addTag("Protocol-Action", "Verify-Identity");
+      tx.addTag("Identity-ID", identity_id);
+      tx.addTag("Content-Type", "text/plain");
+
+      tx.reward = (+tx.reward * 100).toString();
+
+      await arweave.transactions.sign(tx, pk);
+      await arweave.transactions.post(tx);
+
+      if (isVerifiable) {
+        console.log(
+          green(
+            `\n\n--> identity verified: ${arweave_address} <-> ${unevaluatedAddr.exotic_address} || network: ${unevaluatedAddr.ver_req_network}`
+          )
+        );
+        console.log(
+          green(
+            `--> verified identity ID ${identity_id} | verification TXID: ${tx.id}\n\n`
+          )
+        );
+      } else {
+        console.log(
+          red(
+            `\n\n--> verification failed: ${arweave_address} <-> ${unevaluatedAddr.exotic_address} || network: ${userObject.ver_req_network}`
+          )
+        );
+        console.log(
+          red(
+            `--> failed identity ID ${identity_id} | failing verification TXID: ${tx.id}\n\n`
+          )
+        );
+      }
     }
   } catch (error) {
     console.log(error);
