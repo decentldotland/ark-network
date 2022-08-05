5b19bbf74866f4060a0677ae144b86283cce814a
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-08-05 22:53:08 +0300

    feat: Near SC addr & oracle v0.0.9

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,8 @@ import "./setEnv.js";
 // export const ARWEAVE_ORACLE_ADDRESS = "i9Q9Y14HnJUmnSOVdxlPQkHHsT0W6kTv9PXCblZ_kAA"; // v0.0.4
 // export const ARWEAVE_ORACLE_ADDRESS = `jRGJtaBjfvDJgpQATUiW3mBbB_wp71xrUmeQBalrm3k`; // v0.0.6 -- with FUJI Testnet integration
 // export const ARWEAVE_ORACLE_ADDRESS = `44JMzVHrHFKDAf7M0VvqBZT2lXjNbyHrdtVoagx-i28`; // v0.0.7 (testnet ready);
-export const ARWEAVE_ORACLE_ADDRESS = `VWWz1k2u6LnfNLJVCQxVVE3b2ivTVBbgLkMrPe3naoY`; // v0.0.8 (fix telegram verification issue); 
+// export const ARWEAVE_ORACLE_ADDRESS = `VWWz1k2u6LnfNLJVCQxVVE3b2ivTVBbgLkMrPe3naoY`; // v0.0.8 (fix telegram verification issue); 
+export const ARWEAVE_ORACLE_ADDRESS = `5H5Hj81G5j5P2raDhe5VFU-zkf08KDc588GJ8dtlHTw`; // v0.0.9 (exotic hybrid)
 // EVM ADDRESSES
 export const ETH_ORACLE_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`; // Goerli Testnet && ETH Mainnet
 export const AURORA_TESTNET_ADDRESS = `0xfb0200C27185185D7DEe0403D5f102ADb59B7c34`;
@@ -17,6 +18,7 @@ export const FTM_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
 export const OPTIMISM_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
 export const ARBITRUM_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
 export const POLYGON_MAINNET_ADDRESS = `0xdE44d3fB118E0f007f2C0D8fFFE98b994383949A`;
+export const NEAR_MAINNET_ADDRESS = `ark_station_1.near`;
 
 export const EVM_ORACLES_CONTRACTS = [
   ETH_ORACLE_ADDRESS,
