2f9530abcdf0393592bfe314de377a87bbeafaf1
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-11-26 00:40:08 +0100

    perf: handle GQL errors

--- src/utils/arweave/graphql.js
+++ src/utils/arweave/graphql.js
@@ -57,27 +57,32 @@ async function getActionPerAddress(address) {
 }
 
 async function gqlQuery(query) {
-  const response = await axios.post("https://arweave.net/graphql", query, {
-    headers: { "Content-Type": "application/json" },
-  });
-
-  const transactionIds = [];
-
-  const res_arr = response.data.data.transactions.edges;
-
-  for (let element of res_arr) {
-    const tx = element["node"];
-
-    transactionIds.push({
-      id: tx.id,
-      owner: tx.owner.address,
-      // pending transactions do not have block value
-      blockheight: tx.block?.height ? tx.block.height : tx.block?.timestamp,
-      tags: tx.tags ? tx.tags : [],
+  try {
+    const response = await axios.post("https://arweave.net/graphql", query, {
+      headers: { "Content-Type": "application/json" },
     });
-  }
 
-  return transactionIds;
+    const transactionIds = [];
+
+    const res_arr = response.data.data.transactions.edges;
+
+    for (let element of res_arr) {
+      const tx = element["node"];
+
+      transactionIds.push({
+        id: tx.id,
+        owner: tx.owner.address,
+        // pending transactions do not have block value
+        blockheight: tx.block?.height ? tx.block.height : tx.block?.timestamp,
+        tags: tx.tags ? tx.tags : [],
+      });
+    }
+
+    return transactionIds;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
 
 export async function getLastInteractionBlock() {
